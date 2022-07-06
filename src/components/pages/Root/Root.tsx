03264f12e70e9aee0ae9049678d7f624175f90c8
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2022-07-06 09:11:53 +0200

    build: add react project

---
+++ src/components/pages/Root/Root.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OrderFormContainer from '../OrderPage';
+
+function Root() {
+  return (
+    <div className="App">
+      <OrderFormContainer />
+    </div>
+  );
+}
+
+export default Root;
