b75f92eca3cd248e769fb4964711675e158ebdfe
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2022-07-08 14:46:11 +0200

    refactor: change default imports to named imports

--- src/components/pages/OrderPage/OrderPageContainer.tsx
+++ src/components/pages/OrderPage/OrderPageContainer.tsx
@@ -1,5 +1,6 @@
 import { OrderPage } from './OrderPage'
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {}
 
 export const OrderPageContainer: React.FC<Props> = () => {
