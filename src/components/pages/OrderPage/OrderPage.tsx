3b7d0189c15237ddd018362328e819a11be3d3d7
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2022-07-20 14:16:57 +0200

    feat: add FigureSidebar to OrderPage

--- src/components/pages/OrderPage/OrderPage.tsx
+++ src/components/pages/OrderPage/OrderPage.tsx
@@ -1,7 +1,8 @@
 import { Wrapper } from './OrderPage.styles'
 import { IMinifig, IMinifigPart } from './types'
-import { FigureSidebarContainer } from '../../organisms/FigureSidebar'
-import { Title } from '../../atoms/Title'
+import { FigureSidebar } from '../../organisms/FigureSidebar'
+import { FigurePart } from '../../molecules/FigurePart'
+import { Figure } from '../../molecules/Figure'
 
 interface Props {
   minifig: IMinifig | null
@@ -11,9 +12,17 @@ interface Props {
 export const OrderPage: React.FC<Props> = ({ minifig, parts }) => {
   return (
     <Wrapper>
-      <FigureSidebarContainer minifig={minifig} parts={parts}>
-        <Title>Your minifig</Title>
-      </FigureSidebarContainer>
+      <FigureSidebar
+        minifig={minifig && <Figure name={minifig.name} imageUrl={minifig.set_img_url} />}
+        parts={parts.map(({ part }) => (
+          <FigurePart
+            imageUrl={part.part_img_url}
+            name={part.name}
+            id={part.part_num}
+            key={part.part_num}
+          />
+        ))}
+      />
     </Wrapper>
   )
 }
