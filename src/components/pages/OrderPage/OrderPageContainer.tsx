121b3d5217e94d346eb7efd47a4c2e13022dabd1
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2022-07-26 13:43:43 +0200

    feat: add validation schema

--- src/components/pages/OrderPage/OrderPageContainer.tsx
+++ src/components/pages/OrderPage/OrderPageContainer.tsx
@@ -1,14 +1,27 @@
-import { rebrickableApi, rebrickableUrls } from 'api'
-import { useEffect, useState } from 'react'
-import { getRandomOfArray } from 'utils'
-import { OrderPage } from './OrderPage'
-import { IMinifig, IMinifigPart, IOrderFormData } from './types'
+import { rebrickableApi, rebrickableUrls } from 'api';
+import { useEffect, useState } from 'react';
+import * as yup from 'yup';
+import { getRandomOfArray } from 'utils';
+import { OrderPage } from './OrderPage';
+import { IMinifig, IMinifigPart, IOrderFormData } from './types';
 
 export const OrderPageContainer: React.FC = () => {
-  const [allMinifigs, setAllMinifigs] = useState<IMinifig[]>([])
-  const [minifig, setCurrentMinifig] = useState<null | IMinifig>(null)
-  const [minifigParts, setMinifigParts] = useState<IMinifigPart[]>([])
-  const [orderFormData, setOrderFormData] = useState<IOrderFormData>({})
+  const [allMinifigs, setAllMinifigs] = useState<IMinifig[]>([]);
+  const [minifig, setCurrentMinifig] = useState<null | IMinifig>(null);
+  const [minifigParts, setMinifigParts] = useState<IMinifigPart[]>([]);
+  const [orderFormData, setOrderFormData] = useState<IOrderFormData>({});
+
+  const schema = yup.object().shape({
+    name: yup.string().min(1).max(255),
+    surname: yup.string().min(1).max(255),
+    phoneNumber: yup.string(),
+    email: yup.string().email(),
+    dateOfBirth: yup.date().max(new Date().setDate(new Date().getDate() - 1)), // only dates in the past
+    address: yup.string().min(1).max(255),
+    city: yup.string().min(1).max(255),
+    state: yup.string(),
+    zipCode: yup.string().matches(/^[0-9]{5}$|^[0-9]{5}-[0-9]{5}$/gm),
+  });
 
   useEffect(() => {
     const fetchMinifigs = async () => {
@@ -19,29 +32,35 @@ export const OrderPageContainer: React.FC = () => {
             search: 'Harry Potter',
           },
         },
-      )
+      );
 
-      setAllMinifigs(response.data.results)
-    }
+      setAllMinifigs(response.data.results);
+    };
 
-    fetchMinifigs()
-  }, [])
+    fetchMinifigs();
+  }, []);
 
   useEffect(() => {
-    drawMinifig()
-  }, [allMinifigs])
+    drawMinifig();
+  }, [allMinifigs]);
 
   const drawMinifig = async () => {
-    if (allMinifigs.length === 0) return
+    if (allMinifigs.length === 0) return;
 
-    const randomMinifig = getRandomOfArray<IMinifig>(allMinifigs)
-    setCurrentMinifig(randomMinifig)
+    const randomMinifig = getRandomOfArray<IMinifig>(allMinifigs);
+    setCurrentMinifig(randomMinifig);
 
     const response = await rebrickableApi.get<{ results: IMinifigPart[] }>(
       rebrickableUrls.getPartsOfMinifig(randomMinifig.set_num),
-    )
-    setMinifigParts(response.data.results)
-  }
+    );
+    setMinifigParts(response.data.results);
+  };
 
-  return <OrderPage minifig={minifig} parts={minifigParts} orderFormData={orderFormData} />
-}
+  return (
+    <OrderPage
+      minifig={minifig}
+      parts={minifigParts}
+      orderFormData={orderFormData}
+    />
+  );
+};
