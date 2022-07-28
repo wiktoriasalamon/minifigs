0ff08a8501f75a6d4c5566b6e9d51c2043b91983
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2022-07-28 11:39:33 +0200

    test: change test name

--- src/components/pages/Root/Root.test.tsx
+++ src/components/pages/Root/Root.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { Root } from './Root'
 
-test("renders container for whole application with 'App' className", () => {
+test(`renders container for whole application with 'App' className`, () => {
   const { container } = render(<Root />)
   const appContainer = container.getElementsByClassName('App')
   expect(appContainer).toBeDefined()
