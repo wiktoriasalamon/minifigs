07ce0eb9445ab59c8ebba8db51b749798674e694
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2022-08-08 17:04:59 +0200

    build: add package for translations

--- src/components/pages/Root/Root.tsx
+++ src/components/pages/Root/Root.tsx
@@ -2,6 +2,12 @@ import { ThemeProvider } from 'styled-components'
 import { theme } from 'styles/theme'
 import { GlobalStyle } from 'styles/GlobalStyle'
 import { OrderPageContainer } from 'components/pages/OrderPage'
+import { setLocale, setTranslations } from 'react-i18nify'
+import { languages } from 'translations'
+
+// react-i18nify configuration
+setTranslations(languages)
+setLocale('en')
 
 export function Root() {
   return (
---
+++ src/translations/en.ts
@@ -0,0 +1,19 @@
+export const en = {
+  orderPage: {
+    orderForm: {
+      address: 'Address',
+      city: 'City',
+      email: 'Email address',
+      name: 'Name',
+      phoneNumber: 'Phone number',
+      surname: 'Surname',
+      zipCode: 'Zip code',
+    },
+    sidebar: {
+      drawAgain: 'Draw again',
+      order: 'Place an order',
+      partsLabel: 'There are %{count} parts in this minifig:',
+      title: 'Your minifig',
+    },
+  },
+}
