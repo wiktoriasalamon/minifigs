a1accaca70529e22d03b74109f4222d606f642d4
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2022-08-10 22:33:35 +0200

    test: make a test async

--- src/components/pages/Root/Root.test.tsx
+++ src/components/pages/Root/Root.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react'
 import { Root } from './Root'
 
-test('renders learn react link', () => {
+test('renders learn react link', async () => {
   const { container } = render(<Root />)
   const appContainer = container.getElementsByClassName('App')
   expect(appContainer).toBeDefined()
